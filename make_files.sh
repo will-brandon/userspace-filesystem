381cba9f9d6ca8b54a5a7789832ef76270e97884
Will Brandon <brandon.w@northeastern.edu>
2023-12-07 15:27:01 +0000

    tested space overflow

--- make_files.sh
+++ make_files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MNT_ROOT = "/home/wbrandon/cs3650/cs3650-project2/mnt"
-DIR = $MNT_ROOT/hello
+MNT_ROOT="/home/wbrandon/cs3650/cs3650-project2/mnt"
+DIR="${MNT_ROOT}/hello/123/456/789"
 
-mkdir $DIR
+mkdir -p "${DIR}"
 
-#for i in 1 .. 100
-#do
-#    touch "$DIR/$i.txt"
-#done
+for i in {1..1000}
+do
+    touch "${DIR}/${i}.txt"
+done
